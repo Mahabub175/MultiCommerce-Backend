049d9e31d7215a756c82cea54bfae283d05ff0c7
Mahabub Ahmed <121817382+Mahabub175@users.noreply.github.com>
2025-11-21 18:51:51 +0600

    Add auth middleware to order routes
    Added authentication middleware to order retrieval routes.

--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post("/order/", orderControllers.createOrderController);
 
-router.get("/order/", orderControllers.getAllOrderController);
+router.get("/order/", authMiddleware, orderControllers.getAllOrderController);
 
 router.get("/order/:orderId/", orderControllers.getSingleOrderController);
 
@@ -51,5 +51,11 @@ router.patch(
 );
 
 router.get("/order/user/:userId/", orderControllers.getOrdersByUserController);
+
+router.get(
+  "/order/returned-order/get/products/",
+  authMiddleware,
+  orderControllers.getReturnedProductsController
+);
 
 export const orderRoutes = router;
